7cb3d12148ad653416ac60e736b2439be5018c4a
jmorales <jmorales-2018317@kinal.edu.gt>
2023-08-14 15:21:04 -0600

    Menu image fixed

--- app/Carta/page.tsx
+++ app/Carta/page.tsx
@@ -2,7 +2,7 @@ const Carta = () => {
   return (
     <div className="pt-[90px] w-full dark:bg-zinc-900">
 
-      <div className="w-auto bg-cover bg-fixed h-[calc(100vh-30rem)] lg:h-[calc(100vh-15rem)] text-center whitespace-nowrap dark:text-white bg-[url('https://images.unsplash.com/photo-1599172995721-49309fff2f21?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')]">
+      <div className="w-auto bg-cover bg-fixed h-[calc(100vh-30rem)] lg:h-[calc(100vh-15rem)] text-center whitespace-nowrap dark:text-white bg-[url('https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')]">
         <div className="w-full bg-opacity-60 bg-black h-[calc(100vh-30rem)] lg:h-[calc(100vh-15rem)]">
           <div className="flex h-full w-full text-center items-center justify-center text-6xl lg:text-8xl">
             <span className="font-bold text-white pr-2 lg:pr-5">
