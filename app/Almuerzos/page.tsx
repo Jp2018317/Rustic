0474c15891a50ce7adf38cbe535a4af0a7082d18
jmorales <jmorales-2018317@kinal.edu.gt>
2023-08-14 15:18:40 -0600

    Views

--- app/Almuerzos/page.tsx
+++ app/Almuerzos/page.tsx
@@ -1,17 +1,19 @@
-import Image from "next/image";
-
 const Almuerzos = () => {
   return (
     <div className="pt-[90px] w-full dark:bg-zinc-900">
-      <div className="w-auto bg-cover h-[calc(100vh-40rem)] lg:h-[calc(100vh-15rem)] text-center whitespace-nowrap dark:text-white bg-[url('https://images.unsplash.com/photo-1644447381290-85358ae625cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')]">
-        <div className="w-full bg-opacity-60 bg-black h-[calc(100vh-40rem)] lg:h-[calc(100vh-15rem)]">
-          <div className="flex h-full w-full text-center items-center justify-center text-2xl lg:text-6xl">
+
+      <div className="w-auto bg-cover bg-fixed h-[calc(100vh-30rem)] lg:h-[calc(100vh-15rem)] text-center whitespace-nowrap dark:text-white bg-[url('https://images.unsplash.com/photo-1644447381290-85358ae625cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')]">
+        <div className="w-full bg-opacity-60 bg-black h-[calc(100vh-30rem)] lg:h-[calc(100vh-15rem)]">
+          <div className="flex h-full w-full text-center items-center justify-center text-4xl lg:text-8xl">
             <span className="font-bold text-white pr-2 lg:pr-5">
               ALMUERZOS
             </span>
           </div>
         </div>
       </div>
+
+      <div className="w-full h-screen">a</div>
+      
     </div>
   );
 };
--- app/Bebidas/page.tsx
+++ app/Bebidas/page.tsx
@@ -1,11 +1,23 @@
 const Bebidas = () => {
   return (
     <div className="pt-[90px] w-full dark:bg-zinc-900">
-      <div className="w-full h-screen dark:text-white">
-      Bebidas
-      </div>
-    </div>
-  )
-}
 
-export default Bebidas
+      <div className="w-auto bg-cover bg-fixed h-[calc(100vh-30rem)] lg:h-[calc(100vh-15rem)] text-center whitespace-nowrap dark:text-white bg-[url('https://images.unsplash.com/photo-1554624158-c0d08d8dc6df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')]">
+        <div className="w-full bg-opacity-50 bg-black h-[calc(100vh-30rem)] lg:h-[calc(100vh-15rem)]">
+          <div className="flex h-full w-full text-center items-center justify-center text-6xl lg:text-8xl">
+            <span className="font-bold text-white pr-2 lg:pr-5">
+              BEBIDAS
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full h-screen">
+        a
+      </div>
+      
+    </div>
+  );
+};
+
+export default Bebidas;
