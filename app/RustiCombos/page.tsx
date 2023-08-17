2c7d77be9f574cb999035453b7cec502f949d9e1
jmorales <jmorales-2018317@kinal.edu.gt>
2023-08-17 00:28:53 -0600

    RustiCombos image

--- app/RustiCombos/page.tsx
+++ app/RustiCombos/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import banner from "@/public/Images/Banners/EntradasYPostres.jpg"
+import banner from "@/public/Images/Banners/RustiCombos.jpg"
 
 const RustiCombos = () => {
   return (
