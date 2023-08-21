3c6543a869b005a4fa068191503556851c4b0ff8
jmorales <jmorales-2018317@kinal.edu.gt>
2023-08-20 22:35:40 -0600

    montserrat font added

--- fonts.ts
+++ fonts.ts
@@ -1,7 +1,8 @@
-import { Alfa_Slab_One, Raleway } from 'next/font/google'
+import { Alfa_Slab_One, Montserrat, Raleway } from 'next/font/google'
  
 // define your variable fonts
 const alfaSlabOne = Alfa_Slab_One({ weight: "400", subsets: ['latin']})
 const raleway = Raleway({subsets: ['latin']})
+const montserrat = Montserrat({subsets: ['latin']});
  
-export { alfaSlabOne, raleway }
+export { alfaSlabOne, raleway, montserrat }
