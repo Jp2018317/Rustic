e0b63e03a90218f79f972cfbdddf317eb7cf9d1f
jmorales <jmorales-2018317@kinal.edu.gt>
2023-08-26 18:22:25 -0600

    testing change svh to dvh

--- app/Bebidas/page.tsx
+++ app/Bebidas/page.tsx
@@ -8,13 +8,13 @@ import Productos from "../components/Productos";
 const Bebidas = () => {
   return (
     <div className="pt-[90px] w-full dark:bg-zinc-900">
-      <div className="relative h-[calc(100svh-20rem)] sm:h-[calc(100svh-10rem)] min-h-[30rem]">
-        <div className="absolute z-40 h-[calc(100svh-20rem)] sm:h-[calc(100svh-10rem)] min-h-[30rem] inset-0 bg-black opacity-60" />
-        <span className="absolute z-50 flex items-center justify-center w-full h-[calc(100svh-20rem)] sm:h-[calc(100svh-10rem)] min-h-[30rem] text-5xl sm:text-7xl text-white font-bold">
+      <div className="relative h-[calc(100dvh-20rem)] sm:h-[calc(100dvh-10rem)] min-h-[30rem]">
+        <div className="absolute z-40 h-[calc(100dvh-20rem)] sm:h-[calc(100dvh-10rem)] min-h-[30rem] inset-0 bg-black opacity-60" />
+        <span className="absolute z-50 flex items-center justify-center w-full h-[calc(100dvh-20rem)] sm:h-[calc(100dvh-10rem)] min-h-[30rem] text-5xl sm:text-7xl text-white font-bold">
           Bebidas
         </span>
         <div
-          className="relative w-full h-[calc(100svh-20rem)] sm:h-[calc(100svh-10rem)] min-h-[30rem]"
+          className="relative w-full h-[calc(100dvh-20rem)] sm:h-[calc(100dvh-10rem)] min-h-[30rem]"
           style={{ clipPath: "inset(0 0 0 0)" }}
         >
           <div className="fixed w-full h-full left-0 top-0">
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <main className="pt-[90px] w-full dark:bg-zinc-900">
       
-      <div className="relative h-[calc(100svh-25rem)] md:h-[calc(100svh-10rem)]">
-        <div className="absolute z-40 h-[calc(100svh-25rem)] md:h-[calc(100svh-10rem)] inset-0 bg-black opacity-60"></div>
-          <div className="absolute z-50 flex items-center justify-center w-full h-[calc(100svh-25rem)] md:h-[calc(100svh-10rem)]">
+      <div className="relative h-[calc(100dvh-25rem)] md:h-[calc(100dvh-10rem)]">
+        <div className="absolute z-40 h-[calc(100dvh-25rem)] md:h-[calc(100dvh-10rem)] inset-0 bg-black opacity-60"></div>
+          <div className="absolute z-50 flex items-center justify-center w-full h-[calc(100dvh-25rem)] md:h-[calc(100dvh-10rem)]">
             <div>
                 <div className={`text-6xl md:text-9xl text-white ${alfaSlabOne.className}`}>
                   RUSTIC
@@ -18,7 +18,7 @@ export default function Home() {
                 </div>
             </div>
           </div>
-        <div className="relative w-full h-[calc(100svh-25rem)] md:h-[calc(100svh-10rem)]" style={{ clipPath: 'inset(0 0 0 0)'}}>
+        <div className="relative w-full h-[calc(100dvh-25rem)] md:h-[calc(100dvh-10rem)]" style={{ clipPath: 'inset(0 0 0 0)'}}>
           <div className="fixed w-full h-full left-0 top-0">
             <Image
               className="object-cover bottom"
